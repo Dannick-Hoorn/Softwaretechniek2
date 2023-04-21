5b5ce5fc34a49b874c9bd66b7f22ecd87b6cf83c
Dannick Hoorn <s1159965@student.windesheim.nl>
2023-04-21 11:17:05 +0200

    5.13.2 af, bezig met 5.13.3

--- Week 1/Opdracht_5.13.2/Source.cpp
+++ Week 1/Opdracht_5.13.2/Source.cpp
@@ -6,9 +6,18 @@ using namespace std;
 string voeg_samen(const vector<string>& v);
 
 int main() {
-
+	string arr[]{ "aap", "beer", "cobra" };
+	vector<string> vs(arr, arr + 3);
+	cout << voeg_samen(vs) << '.';
 }
 
 string voeg_samen(const vector<string>& v) {
-
+	string result;
+	for (int i = 0; i < v.size(); i++) {
+		result += v[i];
+		if (i != v.size() - 1) {
+			result += "-";
+		}
+	}
+	return result;
 }
---
+++ Week 1/Opdracht_5.13.3/Source.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+void verdubbel(vector<int>& v);
+
+int main() {
+	int a[5] = { 1,2,3,4,5 };
+	vector<int> vi(a, a+5);
+	cout <<  << endl;
+	}
+
+void verdubbel(vector<int>& v) {
+
+}
