92b08f7464de9e77d487a7f8a3ca20fc546a5824
Dannick Hoorn <s1159965@student.windesheim.nl>
2023-04-21 08:59:38 +0200

    Eerste programma

--- Week 1/Code in les/Source.cpp
+++ Week 1/Code in les/Source.cpp
@@ -1,6 +1,17 @@
+// Tel hello en world op en print "hello world"
+
+
 #include <string>
+#include <iostream>
+using namespace std;
 
 int main(){
-   s1 = "Hello";
+ string s1 = "Hello";
+ string s2 = " ";
+ string s3 = "World";
+
+ string s4 = s1 + s2 + s3;
+
+ cout << s4;
 
 }
