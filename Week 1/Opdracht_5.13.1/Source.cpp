b49316018b0fcd82df5f210682b422a65d26d6e5
Dannick Hoorn <s1159965@student.windesheim.nl>
2023-04-21 10:30:29 +0200

    5.13.1 Af, begonnen aan 5.13.2

--- Week 1/Opdracht_5.13.1/Source.cpp
+++ Week 1/Opdracht_5.13.1/Source.cpp
@@ -4,13 +4,13 @@ using namespace std;
 
 void vervang(string& s1, char bron, char doel);
 
-void vervang2(string& s2, char bron, char doel);
+void vervang2(string& s1, char bron, char doel);
 
 int main() {
 	string s1 = "abracadabra";
-	vervang(s1, 'a', 'i');
+	vervang(s1, 'a', 'i');//a
 	cout << s1 << endl;
-	vervang2(s1, 'i', 'e');
+	vervang2(s1, 'i', 'e');//b
 	cout << s1 << endl;
 }
 
@@ -25,6 +25,9 @@ void vervang(string& s1, char bron, char doel) {
 }
 
 //b
-void vervang2(string& s2, char bron, char doel) {
-	string::iterator pos;
+void vervang2(string& s1, char bron, char doel) {
+	int pos;
+	while ((pos = s1.find(bron)) != string::npos) {
+		s1.replace(pos, 1, 1, doel);
+	}
 }
---
+++ Week 1/Opdracht_5.13.2/Source.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+string voeg_samen(const vector<string>& v);
+
+int main() {
+
+}
+
+string voeg_samen(const vector<string>& v) {
+
+}
